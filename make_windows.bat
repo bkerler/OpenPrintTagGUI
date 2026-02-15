6ee370ab24fcfef4acfa9fdbda57395ba9a0a9e8
Bjoern Kerler <info@revskills.de>
2026-02-15 13:33:15 +0100

    Update windows packager settings

--- make_windows.bat
+++ make_windows.bat
@@ -4,5 +4,5 @@ rd /S /Q dist
 pip3 install briefcase
 briefcase create windows
 briefcase build windows
-briefcase package windows
+briefcase package windows -i 8b5f17c056bf5eb66151e95b86831d32cd037dfe
 
