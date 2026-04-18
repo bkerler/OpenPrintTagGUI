dbef051cce6753a34061142f2164634759af00d2
Bjoern Kerler <info@revskills.de>
2026-04-18 20:38:35 +0200

    Update requirements

--- make_macos_dmg.sh
+++ make_macos_dmg.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 export LC_ALL=en_US.utf8
-pip3 install briefcase
+python3 -m pip install briefcase
 rm -rf build/
 rm -rf dist/
 rm -rf logs/
-briefcase create macOS Xcode
-briefcase build macOS Xcode
-briefcase package macOS Xcode
-
+python3 -m briefcase create macOS Xcode
+python3 -m briefcase build macOS Xcode
+python3 -m briefcase package macOS Xcode -i 0FACCD5E48260DDA00C85D517BCAD13F022A28F1
+xcrun notarytool submit dist/OctopusDETool-1.1.4.dmg --keychain-profile briefcase-macOS-PSY43399AM --output-format json
